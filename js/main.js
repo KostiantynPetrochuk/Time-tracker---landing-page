3c4448ab22fd41125477fc8ee51a7713c2a56a20
Костянтин Петрочук <you@example.com>
2022-10-06 23:45:04 +0300

    add adaptive

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 'use strict';
-
+//header fixed
 window.addEventListener('scroll', () => {
     const header = document.querySelector('.header');
     const headerIner = document.querySelector('.header-iner');
+    const anchor = document.querySelector('.anchor');
     if (window.scrollY > 0) {
         header.classList.add('header-fixed');
         headerIner.classList.add('header-fixed');
+        anchor.classList.add('_active');
     } else {
         header.classList.remove('header-fixed');
         headerIner.classList.remove('header-fixed');
+        anchor.classList.remove('_active');
     }
 });
 
-//-------------------------
+//scroll animations
 const animItems = document.querySelectorAll('._anim-items');
-console.log(animItems);
 if (animItems.length > 0) {
     window.addEventListener('scroll', animOnScroll);
     function animOnScroll() {
-        console.log('scroll');
         animItems.forEach(e => {
             const animItem = e;
             const animItemHeight = animItem.offsetHeight;
@@ -59,6 +60,32 @@ checkbox.addEventListener('change', () => {
     }
 });
 
+
+// Burger menu
+const mobileMenu = document.querySelector('.burger-menu');
+const mobileMenuButton = document.querySelector('.header-burger');
+const page = document.querySelector('body');
+const menuLinks = document.querySelectorAll('.burger-menu-item');
+
+menuLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        mobileMenuButton.classList.remove('_active');
+        mobileMenu.classList.remove('_active');
+        page.classList.remove('_active');
+    });
+});
+
+mobileMenuButton.addEventListener('click', () => {
+    if (mobileMenuButton.classList.contains('_active')) {
+        mobileMenuButton.classList.remove('_active');
+        mobileMenu.classList.remove('_active');
+        page.classList.remove('_active');
+    } else {
+        mobileMenuButton.classList.add('_active');
+        mobileMenu.classList.add('_active');
+        page.classList.add('_active');
+    }
+});
 
 
 
